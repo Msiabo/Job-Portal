d435b9f2423b54c893d8b6e026c5bb2e960eab85
Siyabonga Mhlongo <mhlongoquincy@gmail.com>
2025-04-24 08:47:46 +0200

    fixed db

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 
-// Function to connect to db
-const connectDB = async () => {
-    try{
-        mongoose.connection.on(('connected'), ()=> console.log('Database Connected Successfully'))
 
-        await mongoose.connect(`${process.env.MONGO_URI}/career-link`)
-    }
-    catch{
-        console.log('Database Connection Failed')
-    }
-    
-}
-export default connectDB
+ const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
+export default connectDB;
